b77821c6ef4411909f377bac6ed6ac8e10d78f25
Karol <karol.szarek@gmail.com>
2021-02-19 17:56:41 +0100

    fc

---
+++ decode.py
@@ -0,0 +1,20 @@
+
+# path = r'C:\Users\karol\OneDrive\Desktop\Decode\decode.txt'
+# #print(path)
+# with open(path, 'r') as reader:
+#     line = reader.readline()
+#     arr = line.split(' ')
+#     for numb in arr:
+#         print(int(numb, 2))
+#     # Further file processing goes here
+    
+import pkgutil
+import numpy
+import matplotlib
+import scipy
+import pandas 
+import pydoc
+import statsmodels
+from matplotlib.pylab import rcParams
+
+
